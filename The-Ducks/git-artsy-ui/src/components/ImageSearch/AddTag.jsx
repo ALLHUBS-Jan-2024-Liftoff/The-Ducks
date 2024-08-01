f3451ca8402040cd89bbc4a8cda0f08f07b1475c
stacijbrooks <stacibrooksfitness@gmail.com>
2024-07-31 21:35:58 -0500

    AddTag jsx and front end image search

---
+++ The-Ducks/git-artsy-ui/src/components/ImageSearch/AddTag.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+// AddTag component allows users to add a new tag to an image
+const AddTag = ({ addTag }) => {
+  // State variable for the tag name
+  const [tagName, setTagName] = useState('');
+
+  // Handles the form submission to add a tag
+  const handleAddTag = (e) => {
+    e.preventDefault();
+    if (tagName) {
+      // Call the addTag function passed as a prop
+      addTag(tagName);
+      // Clear the input field
+      setTagName('');
+    }
+  };
+
+  return (
+    <form onSubmit={handleAddTag} className="add-tag-form">
+      <input
+        type="text"
+        value={tagName}
+        onChange={(e) => setTagName(e.target.value)}
+        placeholder="Add a tag..."
+      />
+      <button type="submit">Add Tag</button>
+    </form>
+  );
+};
+
+AddTag.propTypes = {
+  addTag: PropTypes.func.isRequired,
+};
+
+export default AddTag;
